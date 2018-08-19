27d130edd69afdf4cbb22e18333003d6841c8f15
Michael Lesniak <mlesniak@micromata.de>
2018-08-19 18:05:42 +0200

    Create object

--- main.js
+++ main.js
@@ -3,13 +3,23 @@ function setup() {
 
 }
 
+class Bar {
+  render() {
+    let h = map(sin(frameCount / 50), -1, 1, 100, -100);
+    fill(255);
+    rect(0, 0, 10, h);
+  }
+}
+
+let bars = [];
+bars.push(new Bar());
+
 function draw() {
   translate(width / 2, height / 2);
   rectMode(CENTER);
 
   background(0);
-  fill(255);
-
-  let h = map(sin(frameCount / 50), -1, 1, 100, -100);
-  rect(0, 0, 10, h);
+  bars.forEach((bar) => {
+    bar.render();
+  })
 }
